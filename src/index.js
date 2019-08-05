023018fb8de34023a4ff56ecbca868c42539277a
Anant <techieanant@gmail.com>
2019-08-05 15:14:49 -0700

    add prod build

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 const express = require('express')
 const cors = require('cors')
 require('dotenv').config();
+const path = require('path')
+
 
 const app = express();
 app.use(cors());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+app.use('/', express.static(path.join(__dirname, '../public')));
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!');
+// });
 
 app.listen(process.env.PORT, () =>
   console.log(`Example app listening on port ${process.env.PORT}!`),
