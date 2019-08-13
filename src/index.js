2d173abfa3e51b85630a9c1a959a304aad796db6
unknown <techieanant@gmail.com>
2019-08-12 23:08:58 -0700

    allow origin

--- src/index.js
+++ src/index.js
@@ -7,6 +7,12 @@ const path = require('path')
 const app = express();
 app.use(cors());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use('/', express.static(path.join(__dirname, '../public')));
 
 // app.get('/', (req, res) => {
